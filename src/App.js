43bb4d6fb604d4b8d9e573e6c8ef123c33bbf4ad
Piyush Sinha <piyush.sinha24@gmail.com>
2020-06-27 05:14:32 +0530

    duplicate prop bug

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 function App() {
   return (
     <Switch>
-        <Route exact path='/' exact component={Landing} />
+        <Route exact path='/' component={Landing} />
         <Route exact path='/user/:id' render={(props)=>(<User {...props}/>)} />
     </Switch>
   );
